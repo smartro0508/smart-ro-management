b45f1be1f9277ae5738efb6cb55a6dcae37c6465
dhineshneo <dhinesjkumar@neophrontech.com>
2026-08-19 10:25:22 +0530

    Correction Commit

--- src/config/env.js
+++ src/config/env.js
@@ -1,18 +1,33 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
 export default {
   port: process.env.PORT || 5000,
-  nodeEnv: process.env.NODE_ENV || 'development',
+  nodeEnv: process.env.NODE_ENV || "development",
   db: {
-    host: process.env.NODE_ENV === 'production' ? process.env.PROD_DB_HOST : (process.env.DEV_DB_HOST || 'localhost'),
-    port: process.env.NODE_ENV === 'production' ? process.env.PROD_DB_PORT : (process.env.DEV_DB_PORT || 3306),
-    name: process.env.NODE_ENV === 'production' ? process.env.PROD_DB_NAME : (process.env.DEV_DB_NAME || 'my_database'),
-    user: process.env.NODE_ENV === 'production' ? process.env.PROD_DB_USER : (process.env.DEV_DB_USER || 'root'),
-    password: process.env.NODE_ENV === 'production' ? process.env.PROD_DB_PASSWORD : (process.env.DEV_DB_PASSWORD || ''),
+    host:
+      process.env.NODE_ENV === "production"
+        ? process.env.MYSQLHOST
+        : process.env.DEV_DB_HOST || "localhost",
+    port:
+      process.env.NODE_ENV === "production"
+        ? process.env.MYSQLPORT
+        : process.env.DEV_DB_PORT || 3306,
+    name:
+      process.env.NODE_ENV === "production"
+        ? process.env.MYSQLDATABASE
+        : process.env.DEV_DB_NAME || "my_database",
+    user:
+      process.env.NODE_ENV === "production"
+        ? process.env.MYSQLUSER
+        : process.env.DEV_DB_USER || "root",
+    password:
+      process.env.NODE_ENV === "production"
+        ? process.env.MYSQLPASSWORD
+        : process.env.DEV_DB_PASSWORD || "",
   },
   jwt: {
-    secret: process.env.JWT_SECRET || 'your_secret_key',
-    expiresIn: process.env.JWT_EXPIRES_IN || '7d',
-  }
+    secret: process.env.JWT_SECRET || "your_secret_key",
+    expiresIn: process.env.JWT_EXPIRES_IN || "7d",
+  },
 };
